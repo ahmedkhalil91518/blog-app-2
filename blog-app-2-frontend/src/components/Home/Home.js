c90a19a85c8f56a001c50b6bdd7a4176acef092b
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-04-26 19:45:08 +0200

    add tags section

--- blog-app-2-frontend/src/components/Home/Home.js
+++ blog-app-2-frontend/src/components/Home/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllPostsAC } from "../../reducers/postsReducer";
 import HomeCSS from "./Home.module.css";
-import { Card, Button } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 
 function Home() {
   const dispatch = useDispatch();
@@ -15,24 +15,30 @@ function Home() {
   }, [dispatch]);
 
   return (
-    <div className={HomeCSS.grid}>
-      {" "}
-      {posts.map((post) => {
-        return (
-          <Card
-            key={post.id}
-            style={{ width: "18rem" }}
-          >
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        );
-      })}{" "}
+    <div className="d-flex flex-row">
+      <div className={HomeCSS.main}>
+        <h1>Posts</h1>
+        <div className={HomeCSS.grid}>
+          {" "}
+          {posts.map((post) => {
+            return (
+              <Card key={post.id}>
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            );
+          })}{" "}
+        </div>
+      </div>
+      <div className={HomeCSS.tags}>
+        <h2>Tags</h2>
+        <div  className={HomeCSS.tagsContainer}>#cars #cats #dogs</div>
+      </div>
     </div>
   );
 }
