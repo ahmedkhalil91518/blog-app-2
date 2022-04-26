44317748f338558b9e1797f51628d00952152385
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-04-26 20:24:16 +0200

    make the footer more resposive

--- blog-app-2-frontend/src/components/Footer/Footer.js
+++ blog-app-2-frontend/src/components/Footer/Footer.js
@@ -4,13 +4,13 @@ import FooterCSS from "./Footer.module.css";
 function Footer() {
   return (
     <div className={FooterCSS.footer}>
-      <div className={FooterCSS.footerRow}>
-       <div className=" my-auto mx-3">Follow us on:</div> 
-        <i className="fa-brands fa-facebook my-auto mx-3 fa-2xl"></i>
-        <i className="fa-brands fa-youtube my-auto mx-3 fa-2xl"></i>
-        <i className="fa-brands fa-github my-auto mx-3 fa-2xl"></i>
+      <div className={FooterCSS.footerRow + " " + "text-center"}>
+        <div className=" my-auto mx-3">Follow us on:</div>
+        <i className={"fa-brands fa-facebook my-auto mx-3 fa-2xl " + FooterCSS.icons}></i>
+        <i className={"fa-brands fa-youtube my-auto mx-3 fa-2xl " + FooterCSS.icons}></i>
+        <i className={"fa-brands fa-github my-auto mx-3 fa-2xl " + FooterCSS.icons}></i>
       </div>
-      <div  className=" my-auto mx-3">2045 all rights reserved</div>
+      <div className=" my-auto mx-3 text-center">2045 all rights reserved</div>
     </div>
   );
 }
