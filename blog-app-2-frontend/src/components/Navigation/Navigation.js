169d3b7321ab0cb057984fc8f0b9b6628b327b61
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-04-26 16:10:34 +0200

    finish the navbar

--- blog-app-2-frontend/src/components/Navigation/Navigation.js
+++ blog-app-2-frontend/src/components/Navigation/Navigation.js
@@ -6,19 +6,44 @@ function Navigation() {
   const [searchOpen, setSearchOpen] = useState(false);
   const handleClick = () => {
     setSearchOpen(!searchOpen);
-    console.log("hi");
   };
   return (
-    <Navbar bg="danger" expand="lg" variant="dark">
+    <Navbar bg="danger" expand="lg" variant="dark" className="w-100">
       <Container className={"d-flex " + NavigationCSS.container}>
         <Navbar.Brand href="#home" className="flex-grow-1">
           Blog App 2
         </Navbar.Brand>
-        <Form.Control
-          type="text"
-          placeholder="Search Here"
-          className={"m-3 " + NavigationCSS.search}
-        />
+        {!searchOpen && (
+          <Form.Control
+            type="text"
+            placeholder="Search Here"
+            className={"m-3 " + NavigationCSS.search}
+          />
+        )}
+        {searchOpen && (
+          <>
+            <Form.Control
+              type="text"
+              placeholder="Search Here"
+              className={NavigationCSS.search + " " + NavigationCSS.searchPopup}
+            />
+            <div className={NavigationCSS.overlay}></div>
+            <Button
+              variant="light"
+              size="sm"
+              className={
+                NavigationCSS.searchButton +
+                " " +
+                NavigationCSS.button +
+                " " +
+                NavigationCSS.cancel
+              }
+              onClick={handleClick}
+            >
+              <i className="fa-solid fa-xmark"></i>{" "}
+            </Button>
+          </>
+        )}
         <Button
           variant="light"
           size="sm"
