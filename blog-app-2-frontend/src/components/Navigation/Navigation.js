60dd7d65594ad9c5cdb02de749616578f34ebbb1
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-04-26 13:56:59 +0200

    add css modules to the navigation components

--- blog-app-2-frontend/src/components/Navigation/Navigation.js
+++ blog-app-2-frontend/src/components/Navigation/Navigation.js
@@ -1,21 +1,35 @@
 import React, { useState } from "react";
-import { Navbar, Container, Nav, Button,Form } from "react-bootstrap";
-import "./Navigation.module.css";
+import { Navbar, Container, Nav, Button, Form } from "react-bootstrap";
+import NavigationCSS from "./Navigation.module.css";
 
 function Navigation() {
   const [searchOpen, setSearchOpen] = useState(false);
+  const handleClick = () => {
+    setSearchOpen(!searchOpen);
+  };
   return (
     <Navbar bg="danger" expand="lg" variant="dark">
-      <Container className="d-flex">
+      <Container className={"d-flex " + NavigationCSS.container}>
         <Navbar.Brand href="#home" className="flex-grow-1">
           Blog App 2
         </Navbar.Brand>
-        <Form.Control type="text" placeholder="Search Here" className="m-3"/>
-        <Button variant="light" size="sm" className="me-3">
+        <Form.Control
+          type="text"
+          placeholder="Search Here"
+          className="m-3 search"
+        />
+        <Button
+          variant="light"
+          size="sm"
+          className={"me-3 " + NavigationCSS.searchButton}
+          onClick={handleClick}
+        >
           <i className="fa-solid fa-magnifying-glass"></i>{" "}
         </Button>
         <Button variant="light" size="sm">
-          <Nav.Link href="#home" className="p-0">Signup/Login</Nav.Link>
+          <Nav.Link href="#home" className="p-0">
+            Signup/Login
+          </Nav.Link>
         </Button>
       </Container>
     </Navbar>
