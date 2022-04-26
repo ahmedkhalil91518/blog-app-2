463d5a5e59fcfdeb4eaf1e871b3df00354dc1460
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-04-26 12:34:10 +0200

    add search button

--- blog-app-2-frontend/src/components/Navigation/Navigation.js
+++ blog-app-2-frontend/src/components/Navigation/Navigation.js
@@ -8,7 +8,10 @@ function Navigation() {
         <Navbar.Brand href="#home" className="flex-grow-1">
           Blog App 2
         </Navbar.Brand>
-        <Button variant="light">
+        <Button variant="light" size="sm" className="mx-2">
+          <i className="fa-solid fa-magnifying-glass"></i>{" "}
+        </Button>
+        <Button variant="light" size="sm">
           <Nav.Link href="#home">Signup/Login</Nav.Link>
         </Button>
       </Container>
