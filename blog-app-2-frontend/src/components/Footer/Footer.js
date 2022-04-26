cae928b2c53bd55b81d522329e867b8c9087bfe6
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-04-26 20:13:24 +0200

    add the footer component

--- blog-app-2-frontend/src/components/Footer/Footer.js
+++ blog-app-2-frontend/src/components/Footer/Footer.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import FooterCSS from "./Footer.module.css";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className={FooterCSS.footer}>
+      <div className={FooterCSS.footerRow}>
+       <div className=" my-auto mx-3">Follow us on:</div> 
+        <i className="fa-brands fa-facebook my-auto mx-3 fa-2xl"></i>
+        <i className="fa-brands fa-youtube my-auto mx-3 fa-2xl"></i>
+        <i className="fa-brands fa-github my-auto mx-3 fa-2xl"></i>
+      </div>
+      <div  className=" my-auto mx-3">2045 all rights reserved</div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
