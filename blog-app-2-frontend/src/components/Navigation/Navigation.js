826b39ff27b50aecf000e2c7f619ef12986ee90e
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-04-26 12:52:10 +0200

    add search input

--- blog-app-2-frontend/src/components/Navigation/Navigation.js
+++ blog-app-2-frontend/src/components/Navigation/Navigation.js
@@ -1,14 +1,17 @@
-import React from "react";
-import { Navbar, Container, Nav, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Navbar, Container, Nav, Button,Form } from "react-bootstrap";
 import "./Navigation.module.css";
+
 function Navigation() {
+  const [searchOpen, setSearchOpen] = useState(false);
   return (
     <Navbar bg="danger" expand="lg" variant="dark">
       <Container className="d-flex">
         <Navbar.Brand href="#home" className="flex-grow-1">
           Blog App 2
         </Navbar.Brand>
-        <Button variant="light" size="sm" className="mx-2">
+        <Form.Control type="text" placeholder="Search Here" className="m-3"/>
+        <Button variant="light" size="sm" className="me-3">
           <i className="fa-solid fa-magnifying-glass"></i>{" "}
         </Button>
         <Button variant="light" size="sm">
