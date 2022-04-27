7b4b364bb8c37ee3b34abd7a0ff8eb634bb18c43
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-04-27 10:52:28 +0200

    add pagination

--- blog-app-2-frontend/src/components/Home/Home.js
+++ blog-app-2-frontend/src/components/Home/Home.js
@@ -2,71 +2,15 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllPostsAC } from "../../reducers/postsReducer";
 import HomeCSS from "./Home.module.css";
-import { Card } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { Rating } from "@mui/material";
+import Pagination from "../Pagination/Pagination";
 
 function Home() {
-  const dispatch = useDispatch();
-  const posts = useSelector((state) => {
-    if (state.search.length > 0) {
-      return state.search;
-    } else {
-      return state.posts;
-    }
-  });
-
-  useEffect(() => {
-    dispatch(getAllPostsAC());
-  }, [dispatch]);
-
   return (
     <div className="d-flex flex-row">
       <div className={HomeCSS.main}>
         <h1>Posts</h1>
-        <div className={HomeCSS.grid}>
-          {" "}
-          {posts.map((post) => {
-            return (
-              <Card key={post.id}>
-                <Card.Header className="d-flex flex-row">
-                  <img
-                    src={post.creatorPic}
-                    alt="creator"
-                    className={HomeCSS.circle}
-                  />{" "}
-                  <div className="d-flex flex-column mx-3">
-                    <div>{post.creatorName}</div>
-                    <div>{post.dateCreated}</div>
-                  </div>
-                </Card.Header>
-                <Card.Body>
-                  <Card.Title>
-                    {" "}
-                    <Link to={`/posts/${post.id}`} className={HomeCSS.links}>
-                      {post.title}
-                    </Link>
-                  </Card.Title>
-                  <Card.Subtitle>
-                    {post.tags.map((tag) => {
-                      return <span>#{tag} </span>;
-                    })}
-                  </Card.Subtitle>
-                  <div>
-                    <Rating
-                      name="simple-controlled"
-                      value={+post.rating}
-                      className="my-2"
-                    />
-                  </div>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            );
-          })}{" "}
+        <div>
+          <Pagination />
         </div>
       </div>
       <div className={HomeCSS.tags}>
---
+++ blog-app-2-frontend/src/components/Pagination/Pagination.js
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllPostsAC } from "../../reducers/postsReducer";
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Rating } from "@mui/material";
+import PaginationCSS from "./Pagination.module.css";
+
+function Pagination({ items }) {
+  const itemsPerPage = 6;
+  // We start with an empty list of items.
+  const [currentItems, setCurrentItems] = useState(null);
+  const [pageCount, setPageCount] = useState(0);
+
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => {
+    if (state.search.length > 0) {
+      return state.search;
+    } else {
+      return state.posts;
+    }
+  });
+
+  useEffect(() => {
+    dispatch(getAllPostsAC());
+  }, [dispatch]);
+  // Here we use item offsets; we could also use page offsets
+  // following the API or data you're working with.
+  const [itemOffset, setItemOffset] = useState(0);
+
+  useEffect(() => {
+    // Fetch items from another resources.
+    const endOffset = itemOffset + itemsPerPage;
+    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+    setCurrentItems(posts.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(posts.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage, posts]);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % posts.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
+
+  return (
+    <div className="d-flex flex-column align-items-center justify-content-center">
+      <div className={PaginationCSS.grid}>
+        {currentItems &&
+          currentItems.map((post) => {
+            return (
+              <Card key={post.id}>
+                <Card.Header className="d-flex flex-row">
+                  <img
+                    src={post.creatorPic}
+                    alt="creator"
+                    className={PaginationCSS.circle}
+                  />{" "}
+                  <div className="d-flex flex-column mx-3">
+                    <div>{post.creatorName}</div>
+                    <div>{post.dateCreated}</div>
+                  </div>
+                </Card.Header>
+                <Card.Body>
+                  <Card.Title>
+                    {" "}
+                    <Link
+                      to={`/posts/${post.id}`}
+                      className={PaginationCSS.links}
+                    >
+                      {post.title}
+                    </Link>
+                  </Card.Title>
+                  <Card.Subtitle>
+                    {post.tags.map((tag) => {
+                      return <span>#{tag} </span>;
+                    })}
+                  </Card.Subtitle>
+                  <div>
+                    <Rating
+                      name="simple-controlled"
+                      value={+post.rating}
+                      className="my-2"
+                    />
+                  </div>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            );
+          })}{" "}
+      </div>
+      <ReactPaginate
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={3}
+        marginPagesDisplayed={2}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakLabel="..."
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+        renderOnZeroPageCount={null}
+      />
+    </div>
+  );
+}
+export default Pagination;
